8b2c764a8f7cd4f5fccfeb17a89ae5987b33c62c
Anurag Bhushan <anuragbhushan.01@gmail.com>
2025-12-04 21:14:38 +0530

    feat: introduce Step3Report.jsx

---
+++ client/src/components/Step3Report.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { FaArrowLeft } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import { motion } from "motion/react"
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
+import jsPDF from "jspdf"
+import autoTable from "jspdf-autotable"
